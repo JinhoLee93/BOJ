4e161ee4447aef7f95f8b75bec137989777b493d
Jinho Lee <jl5027@columbia.edu>
2023-03-17 18:42:14 +0900

    [level 3] Title: 합승 택시 요금, Time: 56.07 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/lv3/72413. 합승 택시 요금/합승 택시 요금.swift
+++ 프로그래머스/lv3/72413. 합승 택시 요금/합승 택시 요금.swift
@@ -1,41 +1,32 @@
 import Foundation
 
 func solution(_ n:Int, _ s:Int, _ a:Int, _ b:Int, _ fares:[[Int]]) -> Int {
-    var graph: [Int: [(Int, Int)]] = [:]
+    var distMap = Array(repeating: Array(repeating: Int.max, count: n + 1), count: n + 1)
+    
     for f in fares {
-        var src = f[0], dest = f[1], cost = f[2]
-        graph[src, default: []].append((dest, cost))
-        graph[dest, default: []].append((src, cost))
+        var src = f[0], dest = f[1], cost = f[2] 
+        distMap[src][dest] = cost
+        distMap[dest][src] = cost
     }
     
-    func dijkstra(_ src: Int) -> [Int] {
-        var distMap = Array(repeating: Int.max, count: n + 1)
-        distMap[src] = 0
-        var q: [(Int, Int)] = [(src, 0)]
-        while !q.isEmpty {
-            var cur = q.removeFirst()
-            var dest = cur.0, cost = cur.1
-            if distMap[dest] < cost {
-                continue
-            }
-            guard let g = graph[dest] else { continue }
-            for n in g {
-                var newDest = n.0, newCost = n.1 + cost
-                if distMap[newDest] < newCost {
-                    continue
+    for i in 1..<n + 1 {
+        distMap[i][i] = 0 
+    }
+    
+    for i in 1..<n + 1 {
+        for j in 1..<n + 1 {
+            for k in 1..<n + 1 {
+                var n1 = distMap[j][i], n2 = distMap[i][k]
+                if n1 != Int.max && n2 != Int.max {
+                    distMap[j][k] = min(distMap[j][k], n1 + n2)
                 }
-                distMap[newDest] = newCost
-                q.append((newDest, newCost))
             }
         }
-        
-        return distMap
     }
     
     var res = Int.max
-    for src in 1..<n + 1 {
-        var distMap = dijkstra(src)
-        var s = distMap[s], a = distMap[a], b = distMap[b]
+    for i in 1..<n + 1 {
+        var s = distMap[i][s], a = distMap[i][a], b = distMap[i][b]
         if s != Int.max && a != Int.max && b != Int.max {
             res = min(res, s + a + b)
         }
