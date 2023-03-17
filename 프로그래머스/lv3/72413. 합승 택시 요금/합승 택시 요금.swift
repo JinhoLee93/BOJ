69aff7520fea52ee87d58835ec4beb36f6885763
Jinho Lee <jl5027@columbia.edu>
2023-03-17 18:20:38 +0900

    [level 3] Title: 합승 택시 요금, Time: 22.11 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/lv3/72413. 합승 택시 요금/합승 택시 요금.swift
+++ 프로그래머스/lv3/72413. 합승 택시 요금/합승 택시 요금.swift
@@ -1,41 +1,41 @@
 import Foundation
 
 func solution(_ n:Int, _ s:Int, _ a:Int, _ b:Int, _ fares:[[Int]]) -> Int {
-    var res = Int.max
-    var graph: [Int:[(Int, Int)]] = [:]
+    var graph: [Int: [(Int, Int)]] = [:]
     for f in fares {
-        var s = f[0], d = f[1], w = f[2]
-        graph[s, default: []].append((d, w))
-        graph[d, default: []].append((s, w))
+        var src = f[0], dest = f[1], cost = f[2]
+        graph[src, default: []].append((dest, cost))
+        graph[dest, default: []].append((src, cost))
     }
     
     func dijkstra(_ src: Int) -> [Int] {
-        var q = [(src, 0)]
-        var distMap = Array(repeating: Int.max, count: n + 1) 
+        var distMap = Array(repeating: Int.max, count: n + 1)
         distMap[src] = 0
-        while !q.isEmpty { 
+        var q: [(Int, Int)] = [(src, 0)]
+        while !q.isEmpty {
             var cur = q.removeFirst()
-            var s = cur.0, w = cur.1
-            if distMap[s] < w {
+            var dest = cur.0, cost = cur.1
+            if distMap[dest] < cost {
                 continue
             }
-            guard let g = graph[s] else { continue }
+            guard let g = graph[dest] else { continue }
             for n in g {
-                var d = n.0, nw = n.1 + w
-                if distMap[d] <= nw {
+                var newDest = n.0, newCost = n.1 + cost
+                if distMap[newDest] < newCost {
                     continue
                 }
-                distMap[d] = nw
-                q.append((d, nw))
+                distMap[newDest] = newCost
+                q.append((newDest, newCost))
             }
         }
         
         return distMap
     }
     
-    for i in 1..<n + 1 {
-        var dist = dijkstra(i)
-        var s = dist[s], a = dist[a], b = dist[b]
+    var res = Int.max
+    for src in 1..<n + 1 {
+        var distMap = dijkstra(src)
+        var s = distMap[s], a = distMap[a], b = distMap[b]
         if s != Int.max && a != Int.max && b != Int.max {
             res = min(res, s + a + b)
         }
