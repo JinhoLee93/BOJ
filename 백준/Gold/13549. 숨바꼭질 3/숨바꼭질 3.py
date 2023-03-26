0d4cd4d8c3de67e4416e31e60bb1d9bef1378701
Jinho Lee <jl5027@columbia.edu>
2023-03-27 01:06:13 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 488 ms, Memory: 64376 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -0,0 +1,30 @@
+import sys
+import copy
+from collections import deque
+from collections import defaultdict
+sys.setrecursionlimit(10**6)
+
+N, K = list(map(int, input().split(" ")))
+
+q = deque([(N, 0)])
+timeTable = {}
+while q:
+    curC, curT = q.popleft()
+    if curC in timeTable.keys():
+        if timeTable[curC] > curT:
+            timeTable[curC] = curT
+        else:
+            continue
+    else:
+        timeTable[curC] = curT
+
+    if curC - 1 >= 0:
+        q.append((curC - 1, curT + 1))
+
+    if curC + 1 < K * 2 + 1:
+        q.append((curC + 1, curT + 1))
+
+    if curC * 2 < K * 2 + 1:
+        q.append((curC * 2, curT))
+
+print(timeTable[K])
