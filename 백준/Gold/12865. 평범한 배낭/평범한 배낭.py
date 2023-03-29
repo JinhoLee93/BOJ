56188746c2f2d57efcd79c9bbed62628c60e68a2
Jinho Lee <jl5027@columbia.edu>
2023-03-29 21:53:31 +0900

    [Gold V] Title: 평범한 배낭, Time: 5344 ms, Memory: 379648 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -0,0 +1,31 @@
+import sys
+import math
+import copy
+from collections import deque
+from collections import defaultdict
+sys.setrecursionlimit(10**6)
+
+N, K = list(map(int, input().split(" ")))
+contents = []
+for _ in range(N):
+    contents.append(list(map(int, input().split(" "))))
+
+contents.sort(key=lambda x:x[0])
+
+def solve():
+    dp = [[0 for _ in range(K + 1)] for _ in range(len(contents) + 1)]
+    maximum = 0
+    for i in range(1, len(contents) + 1):
+        for j in range(1, K + 1):
+            idx = i - 1
+            w = contents[idx][0]
+            v = contents[idx][1]
+            if j - w < 0:
+                dp[i][j] = dp[i - 1][j]
+                continue
+            dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w] + v)
+            maximum = max(maximum, dp[i][j])
+
+    return maximum
+
+print(solve())
