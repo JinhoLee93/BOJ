914f99e6080dfe06dee40a58ccbcb4e014c8f1af
Jinho Lee <jl5027@columbia.edu>
2023-03-30 22:19:19 +0900

    [Gold III] Title: 나무 재테크, Time: 576 ms, Memory: 36436 KB -BaekjoonHub

--- 백준/Gold/16235. 나무 재테크/나무 재테크.py
+++ 백준/Gold/16235. 나무 재테크/나무 재테크.py
@@ -1,67 +1,73 @@
 import sys
+import math
+import copy
 from collections import deque
 from collections import defaultdict
+sys.setrecursionlimit(10**6)
 
 N, M, K = list(map(int, input().split(" ")))
-resources = [] # will be used during the winter
+resources = []
 for _ in range(N):
     resources.append(list(map(int, input().split(" "))))
 
-trees = defaultdict(deque)
+trees = [[{} for _ in range(N)] for _ in range(N)]
 for _ in range(M):
-    tree = list(map(int, sys.stdin.readline().split(" ")))
-    r, c, age = tree[0] - 1, tree[1] - 1, tree[2]
-    trees[(r, c)].append(age)
+    r, c, age = list(map(int, sys.stdin.readline().split(" ")))
+    trees[r - 1][c - 1][age] = 1
 
 board = [[5 for _ in range(N)] for _ in range(N)]
 
-def solve(board, trees):
+def solve():
     for _ in range(K):
-        # Spring, Summer
         for r in range(N):
             for c in range(N):
-                key = (r, c)
-                if key in trees:
-                    r, c = key[0], key[1]
-                    curTrees = trees[key]
-                    breakPoint = -1
-                    nxtTrees = deque()
-                    for i in range(len(curTrees)):
-                        if curTrees[i] > board[r][c]:
-                            breakPoint = i
-                            break
-                        board[r][c] -= curTrees[i]
-                        nxtTrees.append(curTrees[i] + 1)
+                dead = 0
+                if trees[r][c]:
+                    nxtTree = {}
+                    wentBelowZero = False
+                    for age in sorted(trees[r][c].keys()):
+                        if wentBelowZero:
+                            dead += (age // 2) * trees[r][c][age]
+                            continue
+                        if board[r][c] >= age * trees[r][c][age]:
+                            nxtTree[age + 1] = trees[r][c][age]
+                            board[r][c] -= age * trees[r][c][age]
+                        else:
+                            wentBelowZero = True
+                            remaining = board[r][c] // age
+                            if remaining == 0:
+                                dead += (age // 2) * trees[r][c][age]
+                                continue
+                            board[r][c] -= age * remaining
+                            dead += (age // 2) * (trees[r][c][age] - remaining)
+                            nxtTree[age + 1] = remaining
+                    trees[r][c] = nxtTree
+                board[r][c] += resources[r][c] + dead
 
-                    if breakPoint != -1:
-                        for i in range(breakPoint, len(curTrees)):
-                            board[r][c] += curTrees[i] // 2
-                    trees[key] = nxtTrees
-                board[r][c] += resources[r][c]
-
-        # Fall, Winter
         dirs = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
-        newTrees = defaultdict(deque)
-        for key in trees.keys():
-            curTrees = trees[key]
-            curR, curC = key[0], key[1]
-            newTrees[key] += curTrees
-            toPopulate = 0
-            for curTree in curTrees:
-                if curTree % 5 == 0:
-                    toPopulate += 1
-            for dir in dirs:
-                nxtR, nxtC = curR + dir[0], curC + dir[1]
-                if nxtR < 0 or nxtC < 0 or nxtR >= N or nxtC >= N:
-                    continue
-                for _ in range(toPopulate):
-                    newTrees[(nxtR, nxtC)].appendleft(1)
-        trees = newTrees
+        for r in range(N):
+            for c in range(N):
+                if trees[r][c]:
+                    toPopulate = 0
+                    for age in trees[r][c].keys():
+                        if age % 5 == 0:
+                            toPopulate += trees[r][c][age]
+
+                    for d in dirs:
+                        nxtR, nxtC = r + d[0], c + d[1]
+                        if nxtR < 0 or nxtC < 0 or nxtR >= N or nxtC >= N:
+                            continue
+                        if 1 in trees[nxtR][nxtC]:
+                            trees[nxtR][nxtC][1] += toPopulate
+                        else:
+                            trees[nxtR][nxtC][1] = toPopulate
 
     res = 0
-    for key in trees.keys():
-        res += len(trees[key])
+    for r in range(N):
+        for c in range(N):
+            if trees[r][c]:
+                res += sum(trees[r][c].values())
 
     print(res)
 
-solve(board, trees)
+solve()
