e5548564f627a2314199a6d11c4a64bdefdf2dd0
Jinho Lee <jl5027@columbia.edu>
2023-03-31 16:24:39 +0900

    [Gold III] Title: 나무 재테크, Time: 528 ms, Memory: 117436 KB -BaekjoonHub

--- 백준/Gold/16235. 나무 재테크/나무 재테크.py
+++ 백준/Gold/16235. 나무 재테크/나무 재테크.py
@@ -1,72 +1,72 @@
 import sys
-import math
-import copy
-from collections import deque
-from collections import defaultdict
-sys.setrecursionlimit(10**6)
 
 N, M, K = list(map(int, input().split(" ")))
+
 resources = []
 for _ in range(N):
-    resources.append(list(map(int, input().split(" "))))
+    resources.append(list(map(int, sys.stdin.readline().split(" "))))
 
-trees = [[{} for _ in range(N)] for _ in range(N)]
+trees = [[{} for _ in range(N)] for _ in range(N) ]
 for _ in range(M):
-    r, c, age = list(map(int, sys.stdin.readline().split(" ")))
+    r, c, age = list(map(int, input().split(" ")))
     trees[r - 1][c - 1][age] = 1
 
 board = [[5 for _ in range(N)] for _ in range(N)]
 
 def solve():
+    dirs = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
     for _ in range(K):
         for r in range(N):
             for c in range(N):
-                dead = 0
                 if trees[r][c]:
-                    nxtTree = {}
-                    wentBelowZero = False
+                    deadTreesAboutToBeResources = 0
+                    anyResourcesLeft = True
+                    newTree = {}
                     for age in sorted(trees[r][c].keys()):
-                        if wentBelowZero:
-                            dead += (age // 2) * trees[r][c][age]
+                        curResourcesTreesNeed = age * trees[r][c][age]
+                        if not anyResourcesLeft:
+                            deadTreesAboutToBeResources += (age // 2) * trees[r][c][age]
                             continue
-                        if board[r][c] >= age * trees[r][c][age]:
-                            nxtTree[age + 1] = trees[r][c][age]
+                        if curResourcesTreesNeed <= board[r][c]:
                             board[r][c] -= age * trees[r][c][age]
+                            newTree[age + 1] = trees[r][c][age]
                         else:
-                            wentBelowZero = True
-                            remaining = board[r][c] // age
-                            if remaining == 0:
-                                dead += (age // 2) * trees[r][c][age]
+                            ableToFeed = board[r][c] // age
+                            if ableToFeed == 0:
+                                anyResourcesLeft = False
+                                deadTreesAboutToBeResources += (age // 2) * trees[r][c][age]
                                 continue
-                            board[r][c] -= age * remaining
-                            dead += (age // 2) * (trees[r][c][age] - remaining)
-                            nxtTree[age + 1] = remaining
-                    trees[r][c] = nxtTree
-                board[r][c] += resources[r][c] + dead
+                            board[r][c] -= age * ableToFeed
+                            newTree[age + 1] = ableToFeed
+                            remainder = trees[r][c][age] - ableToFeed
+                            deadTreesAboutToBeResources += (age // 2) * remainder
+                    board[r][c] += deadTreesAboutToBeResources
+                    trees[r][c] = newTree
 
-        dirs = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
         for r in range(N):
             for c in range(N):
                 if trees[r][c]:
-                    toPopulate = 0
-                    for age in trees[r][c].keys():
+                    toBePopulated = 0
+                    for age in trees[r][c]:
                         if age % 5 == 0:
-                            toPopulate += trees[r][c][age]
+                            toBePopulated += trees[r][c][age]
 
-                    for d in dirs:
-                        nxtR, nxtC = r + d[0], c + d[1]
-                        if nxtR < 0 or nxtC < 0 or nxtR >= N or nxtC >= N:
-                            continue
-                        if 1 in trees[nxtR][nxtC]:
-                            trees[nxtR][nxtC][1] += toPopulate
-                        else:
-                            trees[nxtR][nxtC][1] = toPopulate
+                    if toBePopulated > 0:
+                        for _ in range(toBePopulated):
+                            for d in dirs:
+                                nxtR, nxtC = r + d[0], c + d[1]
+                                if nxtR < 0 or nxtC < 0 or nxtR >= N or nxtC >= N:
+                                    continue
+                                if 1 in trees[nxtR][nxtC]:
+                                    trees[nxtR][nxtC][1] += 1
+                                else:
+                                    trees[nxtR][nxtC][1] = 1
+                board[r][c] += resources[r][c]
 
     res = 0
     for r in range(N):
         for c in range(N):
-            if trees[r][c]:
-                res += sum(trees[r][c].values())
+            res += sum(trees[r][c].values())
 
     print(res)
 
