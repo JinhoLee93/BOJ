0c42aad312feab2376150cf9c1dc628d6c09e6e4
Jinho Lee <jl5027@columbia.edu>
2023-04-06 17:43:55 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 304 ms, Memory: 115440 KB -BaekjoonHub

--- 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.py
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.py
@@ -1,46 +1,48 @@
 N, K = list(map(int, input().strip().split()))
 belt = list(map(int, input().strip().split()))
 cellsWithRobot = [False] * N
-robots = {}
+robots = []
 busted = 0
 for i in range(2 * N):
     if belt[i] == 0:
         busted += 1
 
-process, robotNumber = 1, 0
+process = 1
 while busted < K:
     # Move the Belt
     toBeAddedAtFirst = belt.pop()
     belt = [toBeAddedAtFirst] + belt
 
-    newRobotsAfterRobotsMove = {}
-    for robot in sorted(robots.keys()):
-        curIdx = robots[robot]
+    toBePopped = 0
+    for i in range(len(robots)):
+        curIdx = robots[i]
         nxtIdx = curIdx + 1
 
         # Move the Belt
         if nxtIdx == N - 1:
             cellsWithRobot[curIdx] = False
+            toBePopped += 1
             continue
         else:
             cellsWithRobot[curIdx] = False
-            robots[robot] = nxtIdx
+            robots[i] = nxtIdx
             cellsWithRobot[nxtIdx] = True
 
-        curIdx = robots[robot]
+        curIdx = robots[i]
         nxtIdx = curIdx + 1
 
         # Move the Robots
         if cellsWithRobot[nxtIdx]:
-            newRobotsAfterRobotsMove[robot] = curIdx
+            robots[i] = curIdx
         else:
             if belt[nxtIdx] > 0:
                 if nxtIdx == N - 1:
+                    toBePopped += 1
                     cellsWithRobot[curIdx] = False
                 else:
                     cellsWithRobot[curIdx] = False
                     cellsWithRobot[nxtIdx] = True
-                    newRobotsAfterRobotsMove[robot] = nxtIdx
+                    robots[i] = nxtIdx
                 belt[nxtIdx] -= 1
                 if belt[nxtIdx] == 0:
                     busted += 1
@@ -48,14 +50,15 @@ while busted < K:
                         print(f"{process}")
                         exit(0)
             else:
-                newRobotsAfterRobotsMove[robot] = curIdx
-    robots = newRobotsAfterRobotsMove
+                robots[i] = curIdx
+
+    for i in range(toBePopped):
+        robots.pop(0)
 
     # Load a Robot
     if belt[0] > 0:
-        robots[robotNumber] = 0
+        robots.append(0)
         cellsWithRobot[0] = True
-        robotNumber += 1
         belt[0] -= 1
         if belt[0] == 0:
             busted += 1
